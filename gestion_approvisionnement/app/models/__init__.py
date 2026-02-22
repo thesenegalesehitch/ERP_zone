5e0707b55ad57a0e3edb7b7fcadec463e860e82d
Alexandre Albert Ndour <aa.ndour5@isepat.edu.sn>
2026-02-22 17:26:51 +0000

    Remove gestion_approvisionnement folder - deleted module no longer needed

--- gestion_approvisionnement/app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.models.supply_order import SupplyOrder
-from app.models.warehouse import Warehouse
--- gestion_approvisionnement/app/schemas/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.schemas.supply_order import SupplyOrderCreate, SupplyOrderResponse
-from app.schemas.warehouse import WarehouseCreate, WarehouseResponse
