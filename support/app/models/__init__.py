d17dce7d48b4a486c97c29a28f762c23b7e969b5
Alexandre Albert Ndour <aa.ndour5@isepat.edu.sn>
2026-02-22 17:39:10 +0000

    Remove support folder - deleted module no longer needed

--- support/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.ticket import Ticket, TicketComment, TicketStatus, TicketPriority
-
-__all__ = ["User", "Ticket", "TicketComment", "TicketStatus", "TicketPriority"]
